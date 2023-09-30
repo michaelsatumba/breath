bfbd6de7852926a54d14b5c60ea242c324668e9f
michaelsatumba <mjsatumba@gmail.com>
2023-09-29 22:58:18 -0700

    added voice

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const circle = document.querySelector('.circle');
 const text = document.getElementById('text');
 const toggleButton = document.getElementById('toggleButton');
+const voiceSelect = document.getElementById('voiceSelect');
 
 let isBreathingIn = true;
 let interval;
+let selectedVoice;
+let voices = [];
 
-// Toggle the breathing session
+let synth = window.speechSynthesis;
+
+// Populate voice list
+function populateVoiceList() {
+  voices = synth.getVoices();
+  voices.forEach((voice, i) => {
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = `${voice.name} (${voice.lang})`;
+    voiceSelect.appendChild(option);
+  });
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+// Voice selection
+voiceSelect.addEventListener('change', (event) => {
+  selectedVoice = voices[event.target.value];
+});
+
+// Speech function
+function speak(text) {
+  let utterThis = new SpeechSynthesisUtterance(text);
+  utterThis.voice = selectedVoice || voices[0];
+  synth.speak(utterThis);
+}
+
+// Toggle breathing
 toggleButton.addEventListener('click', () => {
   if (interval) {
     clearInterval(interval);
@@ -17,16 +50,19 @@ toggleButton.addEventListener('click', () => {
   }
 });
 
+// Breathing logic
 function startBreathing() {
   interval = setInterval(() => {
     if (isBreathingIn) {
       circle.style.width = '200px';
       circle.style.height = '200px';
       text.innerText = 'Breathe Out';
+      speak('Breathe Out');
     } else {
       circle.style.width = '100px';
       circle.style.height = '100px';
       text.innerText = 'Breathe In';
+      speak('Breathe In');
     }
     isBreathingIn = !isBreathingIn;
   }, 2000);
