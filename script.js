8fe3c0a5eb53c93329f59a052d5c4aea5785c34d
michaelsatumba <mjsatumba@gmail.com>
2023-10-01 00:07:33 -0700

    twiglight and avoided complicated logic

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const toggleButton = document.getElementById('toggleButton');
 let isBreathingIn = true;
 let interval;
 
+// Declare time variables
+let startTime = null; // Start time of the current breathe cycle
+let elapsed = 0; // Elapsed time of the current breathe cycle
+
+let animationFrameId;
+
 // Event listener for toggle button
 toggleButton.addEventListener('click', () => {
   breatheInAudio = breatheInAudio || new Audio('BreatheIn.mp3');
@@ -21,14 +27,16 @@ toggleButton.addEventListener('click', () => {
     cancelAnimationFrame(animationFrameId);
     toggleButton.innerText = 'Start';
     interval = null;
+    isBreathingIn = true; // Reset breathing state
+    startTime = null; // Reset start time
+    elapsed = 0; // Reset elapsed time
+    text.innerText = 'Breathe In'; // Reset text
   } else {
     toggleButton.innerText = 'Pause';
     playSound('Breathe In');
     startBreathing();
   }
 });
-
-let animationFrameId;
 
 // Breathing logic
 function startBreathing() {
@@ -37,12 +45,13 @@ function startBreathing() {
   const maxSize = 400; // Maximum size of the circle
   const sizeDifference = maxSize - minSize; // Difference between max and min sizes
 
-  let startTime = null; // Start time of the current breathe cycle
+  // If startTime is not null, calculate new startTime based on saved elapsed time
+  startTime = startTime ? Date.now() - elapsed : null;
 
   function animate(timestamp) {
     if (!startTime) startTime = timestamp; // Initialize start time
 
-    const elapsed = timestamp - startTime; // Calculate elapsed time
+    elapsed = timestamp - startTime; // Calculate elapsed time
 
     // Calculate progress as a value between 0 and 1
     const progress = Math.abs((elapsed % (2 * breatheDuration)) - breatheDuration) / breatheDuration;
@@ -64,7 +73,7 @@ function startBreathing() {
   // Start interval to toggle breathing state and play sound
   interval = setInterval(() => {
     // Decide action based on breathing state
-   const action = isBreathingIn ? 'Breathe Out' : 'Breathe In';
+    const action = isBreathingIn ? 'Breathe Out' : 'Breathe In';
 
     // Update text based on breathing state
     text.innerText = action;
